ccc37aa6c2dddb83f6c766cac5fd392bef95661b
Лидия Хашина <lidiahasina@MacBook-Pro-Lidia.local>
2017-07-21 17:44:02 +0300

    added touch id

--- CryptoWallet/ViewController.swift
+++ CryptoWallet/ViewController.swift
@@ -7,17 +7,53 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBAction func startAuth(_ sender: Any) {
+        let context = LAContext()
+        
+        // Declare a NSError variable.
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [unowned self] (success, authenticationError) in
+                
+                DispatchQueue.main.async {
+                    if success {
+                        self.unlockCryptoWallet()
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "Your fingerprint could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Touch ID not available", message: "Your device is not configured for Touch ID.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func unlockCryptoWallet() {
+        self.performSegue(withIdentifier: "goToSuccessUI", sender: self)
     }
 
 
