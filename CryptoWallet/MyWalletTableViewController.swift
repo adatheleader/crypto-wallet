3a2db17a803aec9d582161220332c717b2b064d5
Лидия Хашина <adatheleader@gmail.com>
2017-11-21 17:18:57 +0300

    show balance in btc and usd on the home screen

--- CryptoWallet/MyWalletTableViewController.swift
+++ CryptoWallet/MyWalletTableViewController.swift
@@ -9,18 +9,28 @@
 import UIKit
 
 class MyWalletTableViewController: UITableViewController {
-
     
     @IBOutlet weak var balanceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.updateBalance()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateBalance() {
+        let amount = AppDelegate.instance().updateAddressBalance(address: AppDelegate.instance().address!)
+        let currency = TLCurrencyFormat.getFiatCurrency()
+        let fiatAmount = TLExchangeRate.instance().fiatAmountStringFromBitcoin(currency, bitcoinAmount: amount!)
+        let amountString = TLCurrencyFormat.getProperAmount(amount!)
+        
+        self.balanceLabel.text = "\(amountString) / \(fiatAmount) USD"
+        print("\(amountString) / \(fiatAmount) USD")
     }
 
     // MARK: - Table view data source
@@ -33,5 +43,16 @@ class MyWalletTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath as IndexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = "Transaction address"
+        cell.detailTextLabel?.text = "-0 BTC"
+        
+        
+        return cell
     }
 }
